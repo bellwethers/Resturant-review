fbb1c014414e0926bf331967c04f5859b056d980
krystin perry <perrykrys@aol.com>
2018-10-30 15:30:55 -0400

    offline view

--- sw.js
+++ sw.js
@@ -2,48 +2,34 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-stuff').then(function(cache) {
       return cache.addAll([
-        '/',
-  				'/index.html',
-  				'/restaurant.html',
-  				'/css/styles.css',
-  				'/data/restaurants.json',
-  				'/img/1.jpg',
-  				'/img/2.jpg',
-  				'/img/3.jpg',
-  				'/img/4.jpg',
-  				'/img/5.jpg',
-  				'/img/6.jpg',
-  				'/img/7.jpg',
-  				'/img/8.jpg',
-  				'/img/9.jpg',
-  				'/img/10.jpg',
-  				'/js/dbhelper.js',
-  				'/js/main.js',
-  				'/js/restaurant_info.js'
-  			]);
+      '/',
+     '/js/main.js',
+     '/img/1.jpg',
+     '/img/2.jpg',
+     '/img/3.jpg',
+     '/img/4.jpg',
+     '/img/5.jpg',
+     '/img/5.jpg',
+     '/img/6.jpg',
+     '/img/7.jpg',
+     '/img/8.jpg',
+     '/img/9.jpg',
+     '/img/10.jpg',
+
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response){
-      if(response){
-      console.log( e.request, ' is in cache');
-      return response;
-    }else{
-      console.log(e.request, ' is not in cache. Fetching...');
-      return fetch (e.request)
-      .then (function(response){
-        caches.open('my-stuff').then (function(cache){
-          cache.put(e.request,response);
-        })
-        return response;
-      })
-      .catch(function(error) {
-					console.log(error);
-				});
-    };
-  })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('my-stuff').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
   );
 });
